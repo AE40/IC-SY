61e2759feab44581611a79f5ef6cd0c48c6a48fe
AE4 <alfred@post.com>
2017-12-17 16:12:49 +0100

    Initial commit

---
+++ Assets/Scripts/Character/Movement.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    // VARS
+
+    bool isWallR;
+    bool isWallL;
+    RaycastHit hitR;
+    RaycastHit hitL;
+	bool canAttach = true;
+	bool hasAttached = false;
+	public float wallRunAngle;
+
+	
+	UnityStandardAssets.Characters.FirstPerson.RigidbodyFirstPersonController cc;
+    Rigidbody rb;
+	public float fallOffForce;
+	[Tooltip("Setting to 1 will disable the boost from jumping off")]
+	public float jumpOffMultiplier;
+	public float jumpOffBoost;
+
+	void Awake( ){
+        cc = GetComponent<UnityStandardAssets.Characters.FirstPerson.RigidbodyFirstPersonController>();
+		rb = GetComponent<Rigidbody>();
+    }
+
+	void Start( ) {
+		
+	}
+	
+	void FixedUpdate( ) {
+
+		if( cc.Grounded ) {
+
+			hasAttached = false;
+			canAttach = true;
+
+		} else {
+			if( Input.GetKeyDown( KeyCode.Space ) ) {
+				AfterRun( fallOffForce * jumpOffMultiplier );
+			} else if ( Input.GetKeyUp( KeyCode.Space ) ) {
+				canAttach = true;
+				hasAttached = false;
+			} else {
+				if( TryAttach() ) {
+
+				} else {
+					if(hasAttached) {
+						AfterRun();
+					}
+				}
+			}
+		}
+	}
+
+	private bool TryAttach( ) {
+		if( canAttach && Input.GetKey( KeyCode.D ) && Physics.Raycast( transform.position,  transform.right, out hitR, 1 ) ) { //user wants to attach on right side (ie. start wallrunning)
+			if( hitR.transform.tag == "Wall" ) { //check if the rayed thing is supposed to be wallrunable
+				isWallR = true;
+				isWallL = false;
+				rb.useGravity = false; //make player fly off into space
+				StablilizeUpwardSpeed(); //make player not fly off into space
+				hasAttached = true;
+				return true;
+			}
+			return false;
+		} else 
+		if( canAttach && Input.GetKey( KeyCode.A ) && Physics.Raycast( transform.position, -transform.right, out hitL, 1 ) ) { //user wants to attach on left side  (ie. start wallrunning)
+			if( hitL.transform.tag == "Wall" ) { //check if the rayed thing is supposed to be wallrunable
+				isWallL = true;
+				isWallR = false;
+				rb.useGravity = false; //make player fly off into space
+				StablilizeUpwardSpeed(); //make player not fly off into space
+				hasAttached = true;
+				return true;
+			}
+			return false;
+		} else {
+			return false;
+		}
+	}
+
+	private void StablilizeUpwardSpeed( ) {
+		rb.velocity = new Vector3( rb.velocity.x, rb.velocity.y * ( 1.1f * Time.fixedDeltaTime ), rb.velocity.z ); //make y velocity divide by 1.1 every second
+	}
+
+	void AfterRun( float force){
+
+		canAttach = false;
+
+		if( isWallL ) {
+			rb.AddForce( (hitL.normal + ( Input.GetKey( KeyCode.W ) ? this.transform.forward : Vector3.zero ) ).normalized * force, ForceMode.VelocityChange );
+		}
+
+		if( isWallR ) {
+			rb.AddForce( ( hitR.normal + ( Input.GetKey( KeyCode.W ) ? this.transform.forward : Vector3.zero ) ).normalized * force, ForceMode.VelocityChange );
+		}
+
+		if (force != fallOffForce ) {
+			rb.AddForce( this.transform.up * jumpOffBoost, ForceMode.VelocityChange );
+		}
+
+		isWallL = false;
+		isWallR = false;
+		rb.useGravity = true;
+	}
+
+	void AfterRun( ) {
+		AfterRun( fallOffForce );
+	}
+
+
+}
---
+++ Assets/Scripts/Character/Respawn.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Respawn : MonoBehaviour {
+
+	private List<Transform> spawnPoints = new List<Transform>();
+	public List<Transform> SpawnPoints {
+		get {
+			return spawnPoints;
+		}
+		internal set {
+			spawnPoints = value;
+		}
+	}
+	private Transform lastSpawnPoint = null;
+
+	void Start () {
+		List<GameObject> gos = GameObject.FindGameObjectsWithTag( "Spawn Point" ).ToList();
+		foreach(GameObject go in gos ) {
+			SpawnPoints.Add( go.transform );
+		}
+	}
+	
+	void Update () {
+		if( Input.GetKeyDown( KeyCode.R ) ) {
+			Transform trans = null;
+			if( SpawnPoints.Count > 0 ) {
+				bool spawnPointAccepted = false;
+				while( !spawnPointAccepted ) {
+					trans = SpawnPoints[Random.Range( 0, SpawnPoints.Count - 1 )];
+
+					if (trans != lastSpawnPoint || SpawnPoints.Count == 1 ) {
+						spawnPointAccepted = true;
+					}
+				}
+			}
+			this.transform.position = trans == null ? new Vector3( 0f, 2f, 0f ) : trans.position;
+			this.GetComponent<Rigidbody>().velocity = Vector3.zero;
+			lastSpawnPoint = trans;
+		}
+	}
+}
